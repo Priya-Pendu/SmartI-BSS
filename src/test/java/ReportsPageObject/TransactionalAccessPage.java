1a69fe7c0086b028de8bc3934428b39a9f15ed77
Priya Pendu <priya.pendu@smartisystems.com>
2026-01-05 17:29:09 +0530

    05/01/2026

--- src/test/java/ReportsPageObject/TransactionalAccessPage.java
+++ src/test/java/ReportsPageObject/TransactionalAccessPage.java
@@ -1,10 +1,17 @@
 package ReportsPageObject;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
 
 import Utilities.BasePage;
+import Utilities.DropDownMethod;
 
 public class TransactionalAccessPage extends BasePage {
 
@@ -14,4 +21,138 @@ public class TransactionalAccessPage extends BasePage {
 	}
 
 	@FindBy(xpath="//p[normalize-space()='Access']") WebElement AccessManagment;
+	@FindBy(xpath="//span[normalize-space()='Reports']") WebElement Reports;
+	@FindBy(xpath="//span[normalize-space()='Transactional']") WebElement Transactional;
+	@FindBy(xpath="//a[@href='/Reports/AccessReport']") WebElement AccessReport;
+	@FindBy(xpath="//div[@id='s2id_BuildingSolutionSuite_Reports_AccessReportGrid0_QuickFilter_LocationMasterId']//b[@role='presentation']") WebElement LocDrop;
+	@FindBy(xpath="//div[@class=\"select2-result-label\"]") List<WebElement> LocOptions;
+	@FindBy(xpath="//div[@class=\"slick-cell l3 r3\"]") WebElement LocationResult;
+	@FindBy(xpath="//div[@id='s2id_BuildingSolutionSuite_Reports_AccessReportGrid0_QuickFilter_DepartmentId']//b[@role='presentation']") WebElement DeptDrop; 
+	@FindBy(xpath="//div[@class=\"select2-result-label\"]") List<WebElement> DeptOptions;
+	@FindBy(xpath="//div[@class=\"slick-cell l4 r4\"]") WebElement DepartmentResult;
+	@FindBy(xpath="//div[@id='s2id_BuildingSolutionSuite_Reports_AccessReportGrid0_QuickFilter_EmployeeMasterID']//b[@role='presentation']") WebElement EmpDrop;
+	@FindBy(xpath="//li[@class=\"select2-results-dept-0 select2-result select2-result-selectable\"]") List<WebElement> EmpOptions;
+	@FindBy(xpath="//div[@class=\"slick-cell l7 r7\"]") WebElement EmployeeResult;
+	@FindBy(xpath="//div[@id='s2id_BuildingSolutionSuite_Reports_AccessReportGrid0_QuickFilter_DeviceLocationId']//b[@role='presentation']") WebElement DevLocDrop;
+	@FindBy(xpath="//li[@class=\"select2-results-dept-0 select2-result select2-result-selectable\"]") List<WebElement> DevLocOptions;
+	@FindBy(xpath="//div[@class=\"slick-cell l8 r8\"]") WebElement DeviceLocationResult;
+	@FindBy(xpath="//a[@class='select2-choice']//span[@role='presentation']") WebElement DevDrop;
+	@FindBy(xpath="//div[@class=\"select2-result-label\"]") List<WebElement> DevOptions;
+	@FindBy(xpath="//div[@class=\"slick-cell l9 r9\"]") WebElement DeviceResult;
+	@FindBy(xpath="//div[@id='s2id_BuildingSolutionSuite_Reports_AccessReportGrid0_QuickFilter_ReaderID']//b[@role='presentation']") WebElement ReaderDrop;
+	@FindBy(xpath="//div[@class=\"select2-result-label\"]") List<WebElement> ReaderOptions;
+	@FindBy(xpath="//div[@class=\"slick-cell l10 r10\"]") WebElement ReaderResult;
+	
+	
+	DropDownMethod ddm = new DropDownMethod();
+	//Action Method
+	public void InitialStep()
+	{
+		AccessManagment.click();
+		Reports.click();
+		Transactional.click();
+		AccessReport.click();
+	}
+	
+	//Location Filter Validation
+	public void LocationFilter(String location) {
+		logger.info("Initial step started");
+		InitialStep();
+		
+		logger.info("Location Drop down clicked");
+		LocDrop.click();
+		
+		logger.info("Selecting location from the drop down");
+		ddm.selectFromDropdown(LocOptions, "Mumbai", location);
+		
+		logger.info("Verify the result of choosing location.");
+		Assert.assertTrue(LocationResult.getText().contains(location), "Location Filter not working");
+	}
+	
+	//TS0148 - Department Filter Validation
+	public void DepartmentFilter(String department)
+	{
+		logger.info("Initial step started");
+		InitialStep();
+		
+		logger.info("Department Drop down clicked");
+		DeptDrop.click();
+		
+		logger.info("Selecting department from the drop down");
+		ddm.selectFromDropdown(DeptOptions, "Software Department", department);
+		
+		logger.info("Verify the result of choosing department.");
+		Assert.assertTrue(DepartmentResult.getText().contains(department), "Department Filter not working");
+	}
+	
+	//TS0149 - Employee Filter Validation
+	public void EmployeeFilter(String employee) {
+		logger.info("Initial step started");
+		InitialStep();
+
+		logger.info("Employee Drop down clicked");
+		EmpDrop.click();
+
+		logger.info("Selecting employee from the drop down");
+		ddm.selectFromDropdown(EmpOptions, "Shreya Sharma", employee);
+
+		logger.info("Verify the result of choosing employee.");
+		Assert.assertTrue(EmployeeResult.getText().contains(employee), "Employee Filter not working");
+	
+	}
+	
+	//TS0150 - Device Location Filter Validation
+	public void DeviceLocationFilter(String devicelocation) throws InterruptedException {
+		logger.info("Initial step started");
+		InitialStep();
+
+		logger.info("Device Location Drop down clicked");
+		DevLocDrop.click();
+
+		Thread.sleep(2000);
+		logger.info("Selecting device location from the drop down");
+		ddm.selectFromDropdown(DevLocOptions, "Mumbai", devicelocation);
+
+		logger.info("Verify the result of choosing device location.");
+		Assert.assertTrue(DeviceLocationResult.getText().contains(devicelocation),
+		"Device Location Filter not working");
+	}
+	
+	//TS0151 - Device Filter Validation
+	public void DeviceFilter(String device) {
+		logger.info("Initial step started");
+		InitialStep();
+
+		logger.info("Device Drop down clicked");
+		driver.findElement(By.cssSelector(".select2-choice")).click();
+
+		logger.info("Selecting device from the drop down");
+		ddm.selectFromDropdown(DevOptions, "BioSlim12", device);
+
+		logger.info("Verify the result of choosing device.");
+		Assert.assertTrue(DeviceResult.getText().contains(device), "Device Filter not working");
+
+	}
+	
+	
+	//TS0152 - Reader Filter Validation
+	public void ReaderFilter(String reader) throws InterruptedException
+	{
+		logger.info("Initial step started");
+		InitialStep();
+		
+		logger.info("Reader Drop down clicked");
+		ReaderDrop.click();
+		
+		logger.info("Selecting reader from the drop down");
+		ddm.selectFromDropdown(ReaderOptions, "BioSlim Reader 1", "Reader1");
+		
+		logger.info("Verify the result of choosing reader.");
+		Thread.sleep(2000);
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scrollTo(document.body.scrollWidth, 0);");
+		Thread.sleep(2000);
+
+		Assert.assertTrue(ReaderResult.getText().contains(reader), "Reader Filter not working");
+	}
 }
--- src/test/java/ReportsTestCases/TransactionalAccessTest.java
+++ src/test/java/ReportsTestCases/TransactionalAccessTest.java
@@ -1,5 +1,83 @@
 package ReportsTestCases;
 
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import LoginTest.LoginPage;
+import ReportsPageObject.CardPoolReportPage;
+import ReportsPageObject.TransactionalAccessPage;
+
 public class TransactionalAccessTest {
 
+	WebDriver driver;
+	TransactionalAccessPage TAP;
+	
+	@BeforeTest(groups="master")
+	public void Login()
+	{
+		 Map<String, Object> prefs = new HashMap<>();
+	     prefs.put("download.default_directory", "D:\\Priya\\BSS_Automation_Files\\Eclipse-Workspace\\SmartISystems-BSS\\SmartI-BSS\\SmartI-BSS\\download");
+	     prefs.put("download.prompt_for_download", false);
+	     prefs.put("download.directory_upgrade", true);
+	     prefs.put("plugins.always_open_pdf_externally", true); // auto-download PDFs
+
+	     ChromeOptions options = new ChromeOptions();
+	     options.setExperimentalOption("prefs", prefs);
+	        
+		driver= new ChromeDriver(options);
+		driver.get("http://192.168.0.42:915");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		TAP = new TransactionalAccessPage(driver);
+		LoginPage lp = new LoginPage(driver);
+		lp.LginCredentials("tata", "Smarti@321");
+		
+	}
+	
+	   //@Test(priority=1, groups="master")
+	   public void TS0147() throws InterruptedException {
+		   
+		   TAP.LocationFilter("Mumbai");
+	    }
+	
+	   
+	   //@Test(priority=2, groups="master")
+		public void TS0148() throws InterruptedException {
+
+			TAP.DepartmentFilter("Software Department");
+		}
+	   
+		//@Test(priority=3, groups="master")
+		public void TS0149() throws InterruptedException {
+			TAP.EmployeeFilter("Shreya Sharma");
+		}
+		
+		//@Test(priority=4, groups="master")
+		public void TS0150() throws InterruptedException {
+			TAP.DeviceLocationFilter("Mumbai");
+		}
+		
+		//@Test(priority=5, groups="master")
+		public void TS0151() throws InterruptedException {
+			TAP.DeviceFilter("BioSlim12");
+		}
+		
+		@Test(priority=6, groups="master")
+		public void TS0152() throws InterruptedException {
+			TAP.ReaderFilter("BioSlim Reader 1");
+		}
+		
+	  @AfterTest (groups="master")
+	  public void teardown()
+	  {
+		driver.quit();
+	  }
 }
